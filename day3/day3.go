da349c601a68d196675bd2da5f7bfae5cd5ee07b
David D. Smith <davidsmith@acm.org>
2020-11-25 15:59:39 -0800

    feat: day3 scaffold and data

---
+++ day3/day3.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/dds/aoc2015/lib/inputs"
+)
+
+var Input = inputs.Day3()
+
+func Test(t *testing.T) {
+	// type test struct {
+	// 	input  int
+	// 	expect int
+	// }
+
+	// tests := []test{
+	// 	test{
+	// 		// ...
+	// 	},
+	// }
+
+	// for i, test := range tests {
+	// 	t.Run(fmt.Sprint(i), func(t *testing.T) {
+	// 		require.Equal(t, test.expect, test.input)
+	// 	})
+	// }
+}
+
+func main() {
+	fmt.Println(part1(Input))
+	fmt.Println(part2(Input))
+}
+
+func part1(input string) (rc int) {
+	fmt.Println(input)
+	return
+}
+
+func part2(input string) (rc int) {
+	return
+}
