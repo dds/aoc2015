bc563aa6ec88e5da27b93a8542e705c2c548ac2e
David D. Smith <davidsmith@acm.org>
2020-11-25 12:05:14 -0800

    day2 p1

--- day2/day2.go
+++ day2/day2.go
@@ -34,8 +34,29 @@ func main() {
 	fmt.Println(part2(Input))
 }
 
-func part1(input [][]int) interface{} {
-	return input
+func paper(l, w, h int) int {
+	var e int
+	x := 2 * l * w
+	y := 2 * w * h
+	z := 2 * h * l
+	e = x
+	if y < e {
+		e = y
+	}
+	if z < e {
+		e = z
+	}
+	return x + y + z + e>>1
+}
+
+func part1(input [][]int) int {
+	rc := 0
+	for _, r := range input {
+		// l, w, h
+		l, w, h := r[0], r[1], r[2]
+		rc += paper(l, w, h)
+	}
+	return rc
 }
 
 func part2(input [][]int) interface{} {
